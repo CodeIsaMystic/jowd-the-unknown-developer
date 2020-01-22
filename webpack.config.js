a178daa5ed5919a11425ed31f2b436c7099856a3
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-01-22 12:12:52 +0100

    finishing setting up the webpack-dev-server plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,27 +12,29 @@ module.exports = {
     path: path.resolve(__dirname, 'app')
   },
   devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html');
+    },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0'
   },
   mode: 'development',
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader?url=false',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: postCSSPlugins
-            }
+    rules: [{
+      test: /\.css$/i,
+      use: [
+        'style-loader',
+        'css-loader?url=false',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: postCSSPlugins
           }
-        ]
-      }
-    ]
+        }
+      ]
+    }]
   }
 };
 // add this short code on the rules:use:
