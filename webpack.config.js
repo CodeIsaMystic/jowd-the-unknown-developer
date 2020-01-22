a7452bda21ae0e6a91dec038f2214fcfb851ab5e
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-01-22 11:51:07 +0100

    adding webpack-dev-server plugin

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,28 @@ module.exports = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000
+  },
   mode: 'development',
-  watch: true,
   module: {
-    rules: [{
-      test: /\.css$/i,
-      use: [
-        'style-loader',
-        'css-loader?url=false',
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: postCSSPlugins
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader?url=false',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: postCSSPlugins
+            }
           }
-        }
-      ]
-    }]
+        ]
+      }
+    ]
   }
 };
 // add this short code on the rules:use:
