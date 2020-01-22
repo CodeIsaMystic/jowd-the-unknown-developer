a11b709ef2f0b545f550ce736bf03c022a9f5fac
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-01-22 12:45:36 +0100

    add some css arch files & install postcss-mixins plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
   require('autoprefixer')
@@ -12,7 +14,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app')
   },
   devServer: {
-    before: function (app, server) {
+    before: function(app, server) {
       server._watch('./app/**/*.html');
     },
     contentBase: path.join(__dirname, 'app'),
@@ -22,19 +24,21 @@ module.exports = {
   },
   mode: 'development',
   module: {
-    rules: [{
-      test: /\.css$/i,
-      use: [
-        'style-loader',
-        'css-loader?url=false',
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: postCSSPlugins
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader?url=false',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: postCSSPlugins
+            }
           }
-        }
-      ]
-    }]
+        ]
+      }
+    ]
   }
 };
 // add this short code on the rules:use:
